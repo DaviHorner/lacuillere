d5b07c9dc731951d37969bc480df36751f97a57d
Davi Horner <davihhcastro@gmail.com>
2016-08-09 09:58:25 -0300

    Add migrate

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,18 @@ Rails.application.routes.draw do
 
   root to: 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants
+  #create crud
+  # Read
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/:id', to: 'restaurants#show'
+  # # Create
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
+  # # Update
+  # get 'restaurants/:id/edit', to: 'restaurants#edit'
+  # patch 'restaurants/:id', to: 'restaurants#update'
+  # # Delete
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
+
 end
